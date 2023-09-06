4f4939cf764b18516cb7cd9c1c202550f9eaa6a7
Muhamman <muhamman.ahmad@conovoinc.com>
2023-09-06 17:49:50 +0500

    changed url

--- web-client/config.js
+++ web-client/config.js
@@ -1,6 +1,6 @@
 // WebSocket server URL
 const API_URL_DEVELOPMENT = "ws://localhost:8000";
-const API_URL_PRODUCTION = "wss://pokergame-backend.azurewebsites.net/api/v1/";
+const API_URL_PRODUCTION = "wss://pokergame-backend.azurewebsites.net:8080/";
 
 // UI settings
 const LS_MODE_TOGGLE_STATE = "LS_MODE_TOGGLE_STATE";
